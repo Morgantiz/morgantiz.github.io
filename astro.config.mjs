72cd41737e06d1e8b58b71233f2561cef23d744a
Carlo Morganti <carloski@live.it>
2025-06-02 20:12:57 +0200

    wip: astro template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://morgantiz.github.io',
-})
+  site: "https://morgantiz.github.io",
+  integrations: [sentry(), spotlightjs(), mdx(), sitemap()],
+});
