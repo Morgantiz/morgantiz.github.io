4b86bb8af6210470432de267e821e4d878af662c
Carlo Morganti <carloski@live.it>
2025-06-10 01:17:29 +0200

    wip: adjusting header, background, dark and light theme switch

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,8 @@
 
 export const SITE_TITLE = "Carlo Morganti";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const HOME_WORDS = [
+  "Robotics Researcher",
+  "Software Developer",
+  "Mechatronics Engineer",
+];
