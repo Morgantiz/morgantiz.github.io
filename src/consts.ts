0ac895c33e6a2130920de1e8728a713e91ea9607
Carlo Morganti <carloski@live.it>
2025-06-24 19:12:00 +0200

    feat: style corrections

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@ export const SITE_TITLE = "Carlo Morganti";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const HOME_WORDS = [
   "Robotics Researcher",
-  "Software Developer",
   "Mechatronics Engineer",
+  "Software Developer",
 ];
